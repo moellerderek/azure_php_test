3f0604f995cbda674b6e250517df2cdaf4264760
Derek Moeller <github@voteforderek.org>
2014-03-13 19:33:37 -0700

    changed die and stuff

--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php
+  
+  if (!isset($_GET['date']) {
+    die("SET A DATE ASSHOLE");
+  }
+      
+  $file_db = new PDO('sqlite:itt.sqlite');
+  $result = $file_db->query('SELECT * FROM charts WHERE date=' . $_GET['date']);
 
-if (!isset($_GET['date']) die("SET A DATE ASSHOLE");
-
-$file_db = new PDO('sqlite:itt.sqlite');
-$result = $file_db->query('SELECT * FROM charts WHERE date=' . $_GET['date']);
-
-foreach ($result as $date) {
-	print_r($date);
-}
-
+  foreach ($result as $date) {
+    print_r($date);
+  }
+      
 ?>
