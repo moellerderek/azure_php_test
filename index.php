9c45c9cb9f59f45f2655839002f54a3a46362462
Derek Moeller <github@voteforderek.org>
2014-03-13 19:45:42 -0700

    removed the bracket on line 9

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
   if ($date < 19200101 OR $date > 20191207) die("WRONG DATE ASSHOLE");
       
   $file_db = new PDO('sqlite:itt.sqlite');
-  $result = $file_db->query('SELECT * FROM charts WHERE date=' . $date]);
+  $result = $file_db->query('SELECT * FROM charts WHERE date=' . $date);
 
   foreach ($result as $resultdate) {
     print_r($resultdate);
