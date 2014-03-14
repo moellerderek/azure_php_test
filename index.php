7f506d1f651b8ad5909a16ea306b6099539adc04
Derek Moeller <derek@goremedy.com>
2014-03-13 19:28:06 -0700

    changed to accept date

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
 
-echo "Our first site yo - and we made a change!!";
+if (!isset($_GET['date']) die("SET A DATE ASSHOLE");
+
+$file_db = new PDO('sqlite:itt.sqlite');
+$result = $file_db->query('SELECT * FROM charts WHERE date=' . $_GET['date']);
+
+foreach ($result as $date) {
+	print_r($date);
+}
 
 ?>
