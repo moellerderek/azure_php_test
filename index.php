9ded9498d5bb1eb03ceb25977903308a478c92a2
Derek Moeller <derek@goremedy.com>
2014-03-16 20:49:34 -0700

    Added result set to be in an HTML table.

--- index.php
+++ index.php
@@ -16,6 +16,36 @@
   $result = $file_db->query('SELECT * FROM charts WHERE date=' . $date);
 
   foreach ($result as $resultdate) {
+      echo "<table border='1'>
+<tr>
+<th>Self</th>
+<th>Feel</th>
+<th>Talk</th>
+<th>Love</th>
+<th>Does</th>
+<th>Grow</th>
+<th>Pull</th>
+<th>Obox</th>
+<th>Spur</th>
+<th>Stop</th>
+</tr>";
+
+while($row = mysqli_fetch_array($result))
+  {
+  echo "<tr>";
+  echo "<td>" . $row['Self'] . "</td>";
+  echo "<td>" . $row['Feel'] . "</td>";
+  echo "<td>" . $row['Talk'] . "</td>";
+  echo "<td>" . $row['Love'] . "</td>";
+  echo "<td>" . $row['Does'] . "</td>";
+  echo "<td>" . $row['Grow'] . "</td>";
+  echo "<td>" . $row['Pull'] . "</td>";
+  echo "<td>" . $row['Obox'] . "</td>";
+  echo "<td>" . $row['Spur'] . "</td>";
+  echo "<td>" . $row['Stop'] . "</td>";  
+  echo "</tr>";
+  }
+echo "</table>";
     print_r($resultdate);
   }
       
