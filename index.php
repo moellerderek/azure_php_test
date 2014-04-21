7773c8a942d22765a0de402ee90964bab175743f
Derek Moeller <derek@goremedy.com>
2014-04-20 21:34:35 -0700

    Fixed index.php

--- index.php
+++ index.php
@@ -18,20 +18,7 @@
 
   foreach ($result as $resultdate) {
     print_r($result)
-    
-    echo'<table>';
-    echo'<tr>'. $row[0]."</tr>"; 
-    echo'<td>'. $row[1]."</td>";
-    echo'<td>'. $row[2].'</td>';
-    echo'<td>'. $row[3].'</td>';
-    echo'<td>'. $row[4]."</td>";
-    echo'<td>'. $row[5].'</td>';
-    echo'<td>'. $row[6].'</td>';
-    echo'<td>'. $row[7]."</td>";
-    echo'<td>'. $row[8].'</td>';
-    echo'<td>'. $row[9].'</td>';
-    echo'<tr>'.'</tr>';
-    echo'</table>';
+
   }
       
       
