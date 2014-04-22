82504048a6a9f4a3b51f9c6e5ba0fd57de83d846
Derek Moeller <derek@goremedy.com>
2014-04-21 20:07:59 -0700

    See if it needed get date there also.

--- index.php
+++ index.php
@@ -13,8 +13,8 @@
   if (!isset($date)) die("SET A DATE ASSHOLE");
   if ($date < 19200101 OR $date > 20191207) die("WRONG DATE ASSHOLE");
       
-  $file_db = new PDO('sqlite:itt.sqlite');
-  $result = $file_db->query('SELECT * FROM charts WHERE date=' . $date);
+	$file_db = new PDO('sqlite:itt.sqlite');
+	$result = $file_db->query('SELECT * FROM charts WHERE date=' . $_GET['date']);
 
   foreach ($result as $resultdate) 
   {
