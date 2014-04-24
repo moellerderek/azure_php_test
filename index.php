d7bdb393f02323e2a940f7cfbc0fda868ec25d67
Derek Moeller <derek@goremedy.com>
2014-04-23 17:49:52 -0700

    See if this solved the DB query issue.

--- index.php
+++ index.php
@@ -12,11 +12,12 @@
   
   if (!isset($date)) die("SET A DATE ASSHOLE");
   if ($date < 19200101 OR $date > 20191207) die("WRONG DATE ASSHOLE");
-      
-	$file_db = new PDO('sqlite:itt.sqlite');
-	$result = $file_db->query('SELECT * FROM charts WHERE date=' . $date);
+    
+	$db = odbc_connect("PDO('sqlite:itt.sqlite'", "", "") or die ("could not connect<br />");  
+	
+	$result = $db->query('SELECT * FROM charts WHERE date=' . $date);
 
-  foreach ($result as $resultdate);
+  foreach ($result as $resultdate)
   {
     print_r($result);
   } 
