5af45b55c28b5998544176fcfcc80e279bee5dd2
Derek Moeller <github@voteforderek.org>
2014-04-26 08:37:10 -0700

    New connection method.

--- index.php
+++ index.php
@@ -12,15 +12,24 @@
   
   if (!isset($date)) die("SET A DATE ASSHOLE");
   if ($date < 19200101 OR $date > 20191207) die("WRONG DATE ASSHOLE");
-      
-	$file_db = new PDO('sqlite:itt.sqlite');
-	$statement = $sqlite->prepare(SELECT * FROM charts WHERE date=".$date);
-	{
-	     $statement->execute();
+  
+if ($db = sqlite_open('itt.sqlite', 0666, $sqliteerror)) { 
+    //sqlite_query($db, 'CREATE TABLE foo (bar varchar(10))');
+    //sqlite_query($db, "INSERT INTO foo VALUES ('fnord')");
+    $result = sqlite_query($db, 'SELECT * FROM charts WHERE date=".$date');
+    var_dump(sqlite_fetch_array($result)); 
 	}
+else {
+    die($sqliteerror);
+	}
+	//$file_db = new PDO('sqlite:itt.sqlite');
+	//$statement = $sqlite->prepare(SELECT * FROM charts WHERE date=".$date);
+	//{
+	//     $statement->execute();
+	//}
 
-	$result = $statement->fetchAll();
-	print_r($result);
+	//$result = $statement->fetchAll();
+print_r($result);
 ?>
   </h1>
   </body>
