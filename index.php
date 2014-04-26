cfc4b1f8361d438a8d97e1692c91e51fe041a702
Derek Moeller <github@voteforderek.org>
2014-04-26 08:58:10 -0700

    Reverted changes.

--- index.php
+++ index.php
@@ -11,11 +11,10 @@
   
   $date = $_GET['date'];
 
-
   if (!isset($date)) die("SET A DATE ASSHOLE");
   if ($date < 19200101 OR $date > 20191207) die("WRONG DATE ASSHOLE");
   
-	file_db = new PDO('sqlite:itt.sqlite');
+	$file_db = new PDO('sqlite:itt.sqlite');
 	$result = $file_db->query('SELECT * FROM charts WHERE date='.$date);
 
 foreach ($result as $resultdate)
