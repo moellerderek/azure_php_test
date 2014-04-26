b6e22de6cf3a7398074ec155381af64c98cb1bea
Derek Moeller <github@voteforderek.org>
2014-04-26 09:05:15 -0700

    Trying to get results to print.

--- index.php
+++ index.php
@@ -14,13 +14,11 @@
   if (!isset($date)) die("SET A DATE ASSHOLE");
   if ($date < 19200101 OR $date > 20191207) die("WRONG DATE ASSHOLE");
   
-	$file_db = new PDO('sqlite:itt.sqlite');
-	$result = $file_db->query('SELECT * FROM charts WHERE date='.$date);
+	$dbh = new PDO('sqlite:itt.sqlite');
+	$sql = 'SELECT * FROM charts WHERE date='.$date;
 
-foreach ($result as $resultdate)
-  {
-    print_r($result);
-  } 
+	echo "<pre>".print_r($dbh->query($sql)->fetchAll(),true)."</pre><br />;
+      
 ?>
   </h1>
   </body>
