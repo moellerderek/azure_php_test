e855c7fd4c3a97210386319b9bfb81055bd81144
Derek Moeller <github@voteforderek.org>
2014-04-26 08:49:09 -0700

    Reverted changes.

--- index.php
+++ index.php
@@ -15,23 +15,13 @@
   if (!isset($date)) die("SET A DATE ASSHOLE");
   if ($date < 19200101 OR $date > 20191207) die("WRONG DATE ASSHOLE");
   
-if ($db = sqlite_open('sqlite:itt.sqlite', 0666, $sqliteerror)) { 
-    //sqlite_query($db, 'CREATE TABLE foo (bar varchar(10))');
-    //sqlite_query($db, "INSERT INTO foo VALUES ('fnord')");
-    $result = sqlite_query($db, 'SELECT * FROM charts WHERE date=".$date');
-    var_dump(sqlite_fetch_array($result)); 
-	}
-else {
-    die($sqliteerror);
-	}
-	//$file_db = new PDO('sqlite:itt.sqlite');
-	//$statement = $sqlite->prepare(SELECT * FROM charts WHERE date=".$date);
-	//{
-	//     $statement->execute();
-	//}
+file_db = new PDO('sqlite:itt.sqlite');
+	$result = $file_db->query("SELECT * FROM charts WHERE date=".$date);
 
-	//$result = $statement->fetchAll();
-print_r($result);
+foreach ($result as $resultdate)
+  {
+    print_r($result);
+  } 
 ?>
   </h1>
   </body>
