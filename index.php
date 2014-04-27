d45ae2e672a41d038bda86db9ffea0439b653525
Derek Moeller <github@voteforderek.org>
2014-04-27 08:33:17 -0700

    Reverted changes.

--- index.php
+++ index.php
@@ -17,10 +17,10 @@
 	$conn = new PDO('sqlite:itt.sqlite');
 	$sql = 'SELECT * FROM charts WHERE date='.$date;
 
-	$array = $conn->query($sql)->fetchAll(),true;
+	//$array = $conn->query($sql)->fetchAll(),true;
       
-	//echo "<pre>".print_r($conn->query($sql)->fetchAll(),true)."</pre><br />";
-	echo "<pre>".print_r($array)."</pre><br />";
+	echo "<pre>".print_r($conn->query($sql)->fetchAll(),true)."</pre><br />";
+	//echo "<pre>".print_r($array)."</pre><br />";
 
 	//include (report.php);
 	
