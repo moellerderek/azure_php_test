a52c357dcc61feb09980fb57fd2b599589d0e5e2
Derek Moeller <github@voteforderek.org>
2014-04-27 08:17:26 -0700

    Changed to report2; and added another result.

--- report.php
+++ report.php
@@ -1,42 +1,61 @@
-$row = array(
-        '0' => 'date',
-        '1' => 'self',
-        '2' => 'feel',
-        '3' => 'talk',
-        '4' => 'love',
-        '5' => 'does',
-        '6' => 'grow',
-  		'7' => 'pull',
-  		'8' => 'obox',
-  		'9' => 'spur',
-  		'10' => 'stop',
-    );
+/**
+ * Translate a result array into a HTML table
+ *
+ * @author      Aidan Lister <aidan@php.net>
+ * @version     1.3.2
+ * @link        http://aidanlister.com/2004/04/converting-arrays-to-human-					readable-tables/
+ * @param      	array  $array      The result (numericaly keyed, associative 
+ 				inner) array.
+ * @param       bool   $recursive  Recursively generate tables for multi-					dimensional arrays
+ * @param       string $null       String to output for blank cells
+ */
 
-    $rows = array($row, $row, $row, $row, $row, $row, $row, $row, $row, $row);
-
-    print array_to_html($rows);
-
-
-function array_to_html($data) {
-    $report = "";
-
-    if (count($data) > 0) {
-
-        $report .= "<table>";
-        $report .= sprintf("<tr><th>%s</th></tr>", join("</th><th>", 				array_keys($data[0])));
-
-        foreach ($data as $row) {
-
-            $report .= "<tr>";
-
-            foreach ($row as $column) {
-                $report .= "<td>$column</td>";
-            }
-            $report .= "</tr>";
-        }
-        $report .= "</table>";
-    } else {
-        $report = "No data";
+function array2table($array, $recursive = false, $null = '&nbsp;')
+{
+    // Sanity check
+    if (empty($array) || !is_array($array)) {
+        return false;
     }
-
-    return $report;
+ 
+    if (!isset($array[0]) || !is_array($array[0])) {
+        $array = array($array);
+    }
+ 
+    // Start the table
+    $table = "<table>\n";
+ 
+    // The header
+    $table .= "\t<tr>";
+    // Take the keys from the first row as the headings
+    foreach (array_keys($array[0]) as $heading) {
+        $table .= '<th>' . $heading . '</th>';
+    }
+    $table .= "</tr>\n";
+ 
+    // The body
+    foreach ($array as $row) {
+        $table .= "\t<tr>" ;
+        foreach ($row as $cell) {
+            $table .= '<td>';
+ 
+            // Cast objects
+            if (is_object($cell)) { $cell = (array) $cell; }
+             
+            if ($recursive === true && is_array($cell) && !empty($cell)) {
+                // Recursive mode
+                $table .= "\n" . array2table($cell, true, true) . "\n";
+            } else {
+                $table .= (strlen($cell) > 0) ?
+                    htmlspecialchars((string) $cell) :
+                    $null;
+            }
+ 
+            $table .= '</td>';
+        }
+ 
+        $table .= "</tr>\n";
+    }
+ 
+    $table .= '</table>';
+    return $table;
+}
