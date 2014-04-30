7ad155187c7926eb4b68c8c87eeb32f1a5a63738
Derek Moeller <github@voteforderek.org>
2014-04-30 07:30:34 -0700

    Undid while statement.

--- index-sqltable.php
+++ index-sqltable.php
@@ -53,19 +53,19 @@
 	//$result = $conn->query($sql)->fetchAll(),true;
 
 //Coverts results into something readable and alterable in HTML/CSS
-	while (array ($conn->query($sql)->fetchAll(),true)
-/*{
-	$bday = $row["date"];
-    $self = $row["self"];
-    $feel = $row["feel"];
-    $talk = $row["talk"];
-    $love = $row["love"];
-    $does = $row["does"];
-    $grow = $row["grow"];
-    $pull = $row["pull"];
-    $obox = $row["obox"];
-    $spur = $row["spur"];
-    $stop = $row["love"];
+//	while (array ($conn->query($sql)->fetchAll(),true)
+//{
+//	$bday = $row["date"];
+//    $self = $row["self"];
+//    $feel = $row["feel"];
+//    $talk = $row["talk"];
+//    $love = $row["love"];
+//    $does = $row["does"];
+//    $grow = $row["grow"];
+//    $pull = $row["pull"];
+//    $obox = $row["obox"];
+//    $spur = $row["spur"];
+//    $stop = $row["love"];
 */}	
 //echo "<div class="mesa">"."<p>".$date."</p>"."</div>";
   		
