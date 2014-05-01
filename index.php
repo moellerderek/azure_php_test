c782431ad465698e27b75fa51b6eaac91ac310d8
Derek Moeller <derek@goremedy.com>
2014-04-30 17:46:32 -0700

    Moved math to own page.

--- index.php
+++ index.php
@@ -41,34 +41,13 @@
 <br>
 
 <?php
-//get request  
-  $date = $_GET['date'];
+	
+	include ('index-sqltable.php')
 
-  if (!isset($date)) die("SET A DATE ASSHOLE");
-  if ($date < 19200101 OR $date > 20191207) die("WRONG DATE ASSHOLE");
-
-//SQL query and record return  
-	$conn = new PDO('sqlite:itt.sqlite');
-	$sql = 'SELECT * FROM charts WHERE date='.$date;
-
-//Coverts results into something readable and alterable in HTML/CSS
-	foreach ($conn->query($sql) as $row)
-	{
-	print "Date".":".$row['date']."\t"."<br>";
-	print "Self".":".$row['self']."\t"."<br>";
-    print "Feel".":".$row['feel']."\t"."<br>";
-    print "Talk".":".$row['talk']."\t"."<br>";
-    print "Love".":".$row['love']."\t"."<br>";
-    print "Does".":".$row['does']."\t"."<br>";
-    print "Grow".":".$row['grow']."\t"."<br>";
-    print "Pull".":".$row['pull']."\t"."<br>";
-    print "Obox".":".$row['obox']."\t"."<br>";
-    print "Spur".":".$row['spur']."\t"."<br>";
-    print "Stop".":".$row['stop']."\t"."<br>";
-	}
 ?>
-
 </body>
+<br>
+
 <footer>
 <?php
 	
