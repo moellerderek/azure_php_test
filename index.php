fb5075ea5e1ed72296304af5a9bff8ec9df98b57
Derek Moeller <derek@goremedy.com>
2014-04-30 17:42:29 -0700

    Moved working code to index.php

--- index.php
+++ index.php
@@ -1,11 +1,44 @@
 <html>
-  <head>
-  <title>Jew are so funny</title>
+<head>
+<title>Jew are so funny</title>
   <link href='http://fonts.googleapis.com/css?family=Nova+Square' rel='stylesheet' type='text/css'>
-  <link href="index.css" rel="stylesheet">
-  </head>
-  <body>
-  <h1>
+  <link href="/css/index.css" rel="stylesheet">
+<!-- Bootstrap core CSS -->
+	<link href="/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Custom styles for this template -->
+	<link href="jumbotron.css" rel="stylesheet">
+	<link href="/css/sticky-footer.css" rel="stylesheet">
+<!-- Just for debugging purposes. Don't actually copy this line! -->
+<!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
+//	<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js">
+//	</script>
+//	<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js">
+//	</script>
+<![endif]-->
+</head>
+
+<body>
+<div class="masthead">
+
+      <?php
+
+        include ("masthead.php");
+
+      ?>
+    </div>
+
+<!-- Main jumbotron for a primary marketing message or call to action -->
+	<div class="jumbotron">
+		<div class="container">
+			<h1>Birthday Calculator</h1>
+			<p>This project is to take a Get Request, and a SQL query and do 				something with them.</p>
+		</div>
+	</div>
+<br>
 
 <?php
 //get request  
@@ -18,40 +51,29 @@
 	$conn = new PDO('sqlite:itt.sqlite');
 	$sql = 'SELECT * FROM charts WHERE date='.$date;
 
-	echo "<pre>".print_r($conn->query($sql)->fetchAll(),true)."</pre><br />";
-	//$result = $conn->query($sql)->fetchAll(),true;
-
 //Coverts results into something readable and alterable in HTML/CSS
-//	while ($row = $conn->query($sql)->fetchAll())
-//{
-//	$bday = $row["date"];
-//    $self = $row["self"];
-//    $feel = $row["feel"];
-//    $talk = $row["talk"];
-//    $love = $row["love"];
-//    $does = $row["does"];
-//    $grow = $row["grow"];
-//    $pull = $row["pull"];
-//    $obox = $row["obox"];
-//    $spur = $row["spur"];
-//    $stop = $row["love"];
-//}	
-//echo "<div class="mesa">"."<p>".$date."</p>"."</div>";
-  		
-//$result = $conn->query($sql)->fetchAll(),true);
-  
-	//print_r($result);
-   
-	//foreach ($result) {
-	//		echo "<pre>";
-	//  	print_r ($result);
-	//  	echo "</pre>"."<br>";
-	//  	}
-
-	//echo "<pre>".print_r($result)."</pre><br />";
-	//echo "<pre>".print_r($array)." \n"."</pre><br />";
-	//include (report.php);
+	foreach ($conn->query($sql) as $row)
+	{
+	print "Date".":".$row['date']."\t"."<br>";
+	print "Self".":".$row['self']."\t"."<br>";
+    print "Feel".":".$row['feel']."\t"."<br>";
+    print "Talk".":".$row['talk']."\t"."<br>";
+    print "Love".":".$row['love']."\t"."<br>";
+    print "Does".":".$row['does']."\t"."<br>";
+    print "Grow".":".$row['grow']."\t"."<br>";
+    print "Pull".":".$row['pull']."\t"."<br>";
+    print "Obox".":".$row['obox']."\t"."<br>";
+    print "Spur".":".$row['spur']."\t"."<br>";
+    print "Stop".":".$row['stop']."\t"."<br>";
+	}
 ?>
-  </h1>
-  </body>
+
+</body>
+<footer>
+<?php
+	
+	include ("footer.php");
+	
+?>
+</footer>
 </html>
